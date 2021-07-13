25ae641a83ce2d843648d77a382b29101ab2df09
Vinicius Flores <viniciusflores379@gmail.com>
2021-07-13 10:59:31 -0300

    :sparkles: Create summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import {Header} from './components/Header'
+import {Dashboard} from './components/Dashboard'
 import { GlobalStyle } from './styles/global'
 export function App() {
   return (
     <>
-      <Header/>
+      <Header />
+      <Dashboard />
       <GlobalStyle/>
     </>
   );
