67d90d2b0cb20f89aa5d26f745301136afc6789c
Vinicius Flores <viniciusflores379@gmail.com>
2021-07-20 18:10:49 -0300

    :sparkles: Upgrade transactions table on create register

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -10,12 +10,27 @@ interface ITransaction {
     createdAt: string;
 }
 
+// interface ITransactionInput {
+//     title: string;
+//     amount: number;
+//     type: string;
+//     category: string;
+// }
+
+type TransactionInput = Omit<ITransaction, 'id' | 'createdAt' >
+
 interface ITransactionsProviderProps {
     children: ReactNode
 }
 
+interface ITransactionsContextData {
+    transactions: ITransaction[];
+    createTransaction: (transaction: TransactionInput) => Promise<void>
+}
 
-export const TransactionsContext = createContext<ITransaction[]>([])
+export const TransactionsContext = createContext<ITransactionsContextData>(
+    {} as ITransactionsContextData,
+)
 
 export function TransactionsProvider({ children}: ITransactionsProviderProps) {
     const [transactions, setTransactions] = useState<ITransaction[]>([])
@@ -24,8 +39,21 @@ export function TransactionsProvider({ children}: ITransactionsProviderProps) {
             .then(response => setTransactions(response.data.transactions))
     }, [])
 
+    async function createTransaction(transactionInput: TransactionInput) {
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date(),
+        });
+        const { transaction } = response.data;
+        setTransactions([
+            ...transactions,
+            transaction
+        ])
+
+    }
+
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{ transactions, createTransaction}}>
             {children}
         </TransactionsContext.Provider>
     )
